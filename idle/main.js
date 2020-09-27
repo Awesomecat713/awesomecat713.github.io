71227a10f8be57e629680107b8729402d05e120a
Awesomecat713 <chloe.peiying.lin@gmail.com>
2020-09-27 10:21:51 -0700

    the fix

--- idle/main.js
+++ idle/main.js
@@ -1,3 +1,10 @@
 console.log("Well, the page loaded, is this good?");
 localStorage.setItem("one", "1");
 console.log(Number(localstorage.getItem("one")));
+
+var ua = window.navigator.userAgent;
+var msie = ua.indexOf('MSIE ');
+if (msie > 0) {
+   // IE 10 or older => return version number
+        alert("This browser does not support localStorage. Your game will not save.");
+	}
