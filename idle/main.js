e4d97a0d95ad71dbe641414fde2b30a23d390b6e
Awesomecat713 <chloe.peiying.lin@gmail.com>
2020-09-27 12:38:53 -0700

    Update main.js

--- idle/main.js
+++ idle/main.js
@@ -9,9 +9,7 @@ if( navigator.userAgent.match(/Android/i)
  || navigator.userAgent.match(/iPod/i)
  || navigator.userAgent.match(/BlackBerry/i)
  || navigator.userAgent.match(/Windows Phone/i)
-{
 	alert("Sorry! Idle doesn't support mobile yet.")
-}
 else
 {
 	// Get the user-agent string 
